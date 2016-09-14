48b8d079e20f8594798d09880b63717ba1f35a5e
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-09-14 12:54:39 +0300

    Initial commit

---
+++ download-latest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#get library versions
+. versions.sh
+
+#downloader function
+function download () {
+  if [ ! -e "${1}" ]
+  then
+  echo "Downloding ${1}"
+  if wget -O ${1} ${2}
+    then
+        echo "${1} Downloaded"
+    else
+        echo "Error Downloading: ${1}"
+        rm ${1}
+        exit 1
+  fi
+  fi
+}
+
+download "${OPENSSL_TARBALL}" "https://www.openssl.org/source/${OPENSSL_TARBALL}"
+
+download "${LIB_BOOST_TARBALL}" "http://sourceforge.net/projects/boost/files/boost/${LIB_BOOST_VERSION}/${LIB_BOOST_TARBALL}/download"
+
+download "${LIBTORRENT_TARBALL}" "https://github.com/arvidn/libtorrent/archive/${LIBTORRENT_TARBALL}"
+
+download "${ODB_COMPILER_OSX}" "http://www.codesynthesis.com/download/odb/2.4/${ODB_COMPILER_OSX}"
+
+download "${ODB_COMPILER_LINUX}" "http://www.codesynthesis.com/download/odb/2.4/${ODB_COMPILER_LINUX}"
+
+download "${ODB_RUNTIME}" "http://www.codesynthesis.com/download/odb/2.4/${ODB_RUNTIME}"
+
+download "${ODB_SQLITE_RUNTIME}" "http://www.codesynthesis.com/download/odb/2.4/${ODB_SQLITE_RUNTIME}"
+
+download "${LIBPNG_TARBALL}" "ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/${LIBPNG_TARBALL}"
+
+download "${SQLITE_TARBALL}" "https://www.sqlite.org/2015/${SQLITE_TARBALL}"
+
+download "${ZLIB_TARBALL}" "http://zlib.net/${ZLIB_TARBALL}"
---
+++ versions.sh
@@ -0,0 +1,23 @@
+export LIB_BOOST_VER="boost_1_59_0"
+export LIB_BOOST_TARBALL="${LIB_BOOST_VER}.tar.gz"
+export LIB_BOOST_VERSION="1.59.0"
+
+export LIBTORRENT_VERSION="libtorrent-1_1"
+export LIBTORRENT_TARBALL="${LIBTORRENT_VERSION}.tar.gz"
+
+export OPENSSL_VERSION="openssl-1.0.2h"
+export OPENSSL_TARBALL="${OPENSSL_VERSION}.tar.gz"
+
+export LIBPNG_VERSION="libpng-1.6.25"
+export LIBPNG_TARBALL="${LIBPNG_VERSION}.tar.gz"
+
+export ZLIB_VERSION="zlib-1.2.8"
+export ZLIB_TARBALL="${ZLIB_VERSION}.tar.gz"
+
+export SQLITE_VERSION="sqlite-autoconf-3090200"
+export SQLITE_TARBALL="${SQLITE_VERSION}.tar.gz"
+
+export ODB_COMPILER_OSX="odb-2.4.0-i686-macosx.tar.bz2"
+export ODB_COMPILER_LINUX="odb_2.4.0-1_amd64.deb"
+export ODB_RUNTIME="libodb-2.4.0.tar.bz2"
+export ODB_SQLITE_RUNTIME="libodb-sqlite-2.4.0.tar.bz2"
